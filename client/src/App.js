b201de5b1dbcd410da18a86b7aaef9d63bf92570
Juan <coyo_gp@hotmail.com>
2022-08-19 19:33:23 -0300

    0.0.5

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Layout, Menu, Form, Input } from 'antd'
+
+import 'antd/dist/antd.min.css'
+import './App.css'
+import './pages/Home/Home.scss'
+import './layouts/LayoutHome/LayoutHome.scss'
+import './components/MenuTop/MenuTop.scss'
+import './components/Home/LoginForm/LoginForm.scss'
+
+import ToDoLogo from './assets/img/png/to-do-logo.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <LayoutHome />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+const Home = () => {
+  return (
+    <div className="home">
+      <LoginForm />
+      <img width={400} src={ToDoLogo} alt="Logo-to-do-list" />
+    </div>
+  )
+}
+
+const LoginForm = () => {
+  return (
+    <div className="login-form-div">
+      <Form className="login-form">
+        <Form.Item>
+          <Input placeholder="Nombre de usuario" />
+        </Form.Item>
+        <Form.Item>
+          <Input placeholder="Contraseña" />
+        </Form.Item>
+      </Form>
+    </div>
+  )
+}
+
+const LayoutHome = () => {
+  const { Header, Content, Footer } = Layout
+  return (
+    <Layout className="layout-home">
+      <Header className="layout-home__header">
+        <MenuTop />
+      </Header>
+      <Content className="layout-home__content">
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Content>
+      <Footer className="layout-home__footer">Juan - 2022</Footer>
+    </Layout>
+  )
+}
+
+const MenuTop = () => {
+  return (
+    <Menu
+      className="menu-top"
+      mode="horizontal"
+      theme="dark"
+      defaultSelectedKeys={['/home']}
+    >
+      <Menu.Item key="/home">
+        <Link to="/home">Home</Link>
+      </Menu.Item>
+      <Menu.Item key="/tasks">
+        <Link to="/tasks">Todas las tareas</Link>
+      </Menu.Item>
+      <Menu.Item key="/categories">
+        <Link to="/categories">Todas las categorías</Link>
+      </Menu.Item>
+      <Menu.Item key="/task">
+        <Link to="/task">Nueva tarea</Link>
+      </Menu.Item>
+      <Menu.Item key="/category">
+        <Link to="/category">Nueva categoría</Link>
+      </Menu.Item>
+    </Menu>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+// import 
+
+const Home = () => {
+    return (
+        <div>
+            <h3>Soy Home</h3>
+        </div>
+    )
+}
+
+export default Home
